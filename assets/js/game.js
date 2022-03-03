151ce3db7da20b6caae73013543ae82d35a1a7cd
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-02 22:22:11 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth)
 
@@ -12,6 +13,11 @@ var fight = function() {
     //Alert players that they are starting the round
     window.alert ("Welcome to Robot Glatiators!")
 
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+
+    //if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT"){
+    
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
     //Log a resulting message to the console so we know that it worked.
@@ -38,6 +44,25 @@ var fight = function() {
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.")
     }
-};
+  } else if (promptFight === "skip" || promptFight === "SKIP"){
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+        window.alert(playerName + " has chosen to skip the fight! Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+        }
+        //if no (false), ask question again by running fight() again
+        else {
+            fight()
+        }
+    } else {
+    window.alert("You need to choose a valid option. Try again!");
+     fight()
+    }
+}
+
 
 fight();
