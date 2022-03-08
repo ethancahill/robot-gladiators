5d1c9fc5a4e78262befe518e0750d8ad474ea56c
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-08 05:53:16 -0500

    added integer values to store

--- assets/js/game.js
+++ assets/js/game.js
@@ -122,30 +122,23 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE', to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     )
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "refill":
-        case "REFILL":
-        case "Refill":
-    
+        case 1:
            playerInfo.refillHealth();
            break;
 
-        case "upgrade":
-        case "UPGRADE":
-        case "Upgrade":
-    
+        case 2:
             playerInfo.upgradeAttack();
             break;
             
-        case "leave":
-        case "Leave":
-        case "LEAVE":
-            
+        case 3:  
             window.alert("Leaving the store.");
-    
             break;
+
         default:
             window.alert("You did not pick a valid option. Try again.")
             shop();
